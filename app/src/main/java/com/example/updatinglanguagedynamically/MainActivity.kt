ba28a601be594c27c4266b64e8d133823b4ecff7
Vivek995378 <42120703118@gecdelhi.ac.in>
2022-11-17 10:07:36 +0530

    Storing Json file locally and getting data locally to update language of app dynamically

--- app/src/main/java/com/example/updatinglanguagedynamically/MainActivity.kt
+++ app/src/main/java/com/example/updatinglanguagedynamically/MainActivity.kt
@@ -2,26 +2,25 @@ package com.example.updatinglanguagedynamically
 
 import android.os.Build
 import android.os.Bundle
-import android.util.JsonReader
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatButton
 import com.google.gson.Gson
-import com.google.gson.JsonParser
 import dev.b3nedikt.restring.Restring
 import dev.b3nedikt.reword.Reword.reword
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
+import java.io.*
 import java.util.*
+
 
 class MainActivity : BaseActivity() {
 
     lateinit var btnEnglish: AppCompatButton
     lateinit var btnHindi: AppCompatButton
     lateinit var btnPunjabi: AppCompatButton
+    lateinit var btnLocal: AppCompatButton
     lateinit var englishLocale: Locale
     lateinit var hindiLocale: Locale
     lateinit var punjabiLocale: Locale
@@ -39,19 +38,26 @@ class MainActivity : BaseActivity() {
         btnPunjabi = findViewById(R.id.btn_punjabi)
 
         configureLocales()
-        updateStringsHashmap()
 
         btnEnglish.setOnClickListener {
+            writeToEnglishFile("en.json")
+            updateStringsHashmap("en")
             updateAppLanguage("en")
         }
 
         btnHindi.setOnClickListener {
+            writeToHindiFile("hi.json")
+            updateStringsHashmap("hi")
             updateAppLanguage("hi")
         }
 
         btnPunjabi.setOnClickListener {
+            writeToPunjabiFile("pun.json")
+            updateStringsHashmap("pun")
             updateAppLanguage("pun")
         }
+
+
     }
 
     private fun configureLocales() {
@@ -61,16 +67,25 @@ class MainActivity : BaseActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.KITKAT)
-    fun updateStringsHashmap() {
-        getLocalStringJsonHashmap("en").forEach {
-            englishStringsMap[it.key] = it.value
+    fun updateStringsHashmap(language: String) {
+        when (language) {
+            "en" -> {
+                getLocalStringJsonHashmap("en").forEach {
+                    englishStringsMap[it.key] = it.value
+                }
+            }
+            "hi" -> {
+                getLocalStringJsonHashmap("hi").forEach {
+                    hindiStringsMap[it.key] = it.value
+                }
+            }
+            else -> {
+                getLocalStringJsonHashmap("pun").forEach {
+                    punjabiStringsMap[it.key] = it.value
+                }
+            }
         }
-        getLocalStringJsonHashmap("hi").forEach {
-            hindiStringsMap[it.key] = it.value
-        }
-        getLocalStringJsonHashmap("pun").forEach {
-            punjabiStringsMap[it.key] = it.value
-        }
+
     }
 
     private fun updateAppLanguage(language: String) {
@@ -100,22 +115,106 @@ class MainActivity : BaseActivity() {
     fun getLocalStringJsonHashmap(language: String): HashMap<String, String> {
         var listTypeJson: HashMap<String, String> = HashMap()
         try {
-            applicationContext.assets.open("$language.json").use { inputStream ->
+            val targetStream: InputStream = readFromFile("$language.json")
 
-                val streamReader = BufferedReader(InputStreamReader(inputStream, "UTF-8"))
-                val responseStrBuilder = StringBuilder()
+            val streamReader = BufferedReader(InputStreamReader(targetStream, "UTF-8"))
+            val responseStrBuilder = StringBuilder()
 
-                var inputStr: String?
-                while (streamReader.readLine().also { inputStr = it } != null)
-                    responseStrBuilder.append(inputStr)
+            var inputStr: String?
+            while (streamReader.readLine().also { inputStr = it } != null)
+                responseStrBuilder.append(inputStr)
 
-                val jsonObject = JSONObject(responseStrBuilder.toString())
+            val jsonObject = JSONObject(responseStrBuilder.toString())
 
-                listTypeJson = Gson().fromJson(jsonObject.toString(), java.util.HashMap::class.java) as HashMap<String, String>
-            }
+            listTypeJson = Gson().fromJson(
+                jsonObject.toString(),
+                java.util.HashMap::class.java
+            ) as HashMap<String, String>
+
         } catch (exception: IOException) {
+            exception.toString()
         }
         return listTypeJson
     }
 
+    fun writeToEnglishFile(fileName: String) {
+        try {
+            val json = JSONObject()
+            json.put("app_name", "UpdatingLanguageDynamically")
+            json.put("mydownloads", "My Downloads")
+            json.put("title_activity_gklist", "Title")
+            json.put("general_instructions", "General Instructions")
+            json.put("current_affairs", "Current Affairs")
+            json.put("title_activity_alertlist", "Job Alerts")
+            json.put("settings", "Settings")
+            json.put("download", "Download")
+            json.put("magazine", "Magazine")
+            val jsonString = json.toString()
+            val fos = applicationContext.openFileOutput(fileName, MODE_PRIVATE)
+            fos.write(jsonString.toByteArray())
+            fos.close()
+            Toast.makeText(this, "Wrote to file: " + fileName, Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Log.d("moengage", e.toString())
+            e.printStackTrace()
+        }
+    }
+
+    fun writeToHindiFile(fileName: String) {
+        try {
+            val json = JSONObject()
+            json.put("app_name", "UpdatingLanguageDynamically")
+            json.put("mydownloads", "मेरे डाउनलोड")
+            json.put("title_activity_gklist", "शीर्षक")
+            json.put("general_instructions", "सामान्य अनुदेश")
+            json.put("current_affairs", "कर्रेंट अफेयर्स")
+            json.put("title_activity_alertlist", "जॉब अलर्")
+            json.put("settings", "सेटिंग")
+            json.put("download", "डाउनलोड")
+            json.put("magazine", "मैगज़ीन")
+            val jsonString = json.toString()
+            val fos = applicationContext.openFileOutput(fileName, MODE_PRIVATE)
+            fos.write(jsonString.toByteArray())
+            fos.close()
+            Toast.makeText(this, "Wrote to file: " + fileName, Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Log.d("moengage", e.toString())
+            e.printStackTrace()
+        }
+    }
+
+    fun writeToPunjabiFile(fileName: String) {
+        try {
+            val json = JSONObject()
+            json.put("app_name", "UpdatingLanguageDynamically")
+            json.put("mydownloads", "আমার ডাউনলোডগুলি")
+            json.put("title_activity_gklist", "Title")
+            json.put("general_instructions", "সাধারণ নির্দেশাবলী")
+            json.put("current_affairs", "বর্তমান ব্যাপার")
+            json.put("title_activity_alertlist", "চাকরির সতর্কতা")
+            json.put("settings", "সেটিংস")
+            json.put("download", "ডাউনলোড")
+            json.put("magazine", "ম্যাগাজিন")
+            val jsonString = json.toString()
+            val fos = applicationContext.openFileOutput(fileName, MODE_PRIVATE)
+            fos.write(jsonString.toByteArray())
+            fos.close()
+            Toast.makeText(this, "Wrote to file: " + fileName, Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Log.d("moengage", e.toString())
+            e.printStackTrace()
+        }
+    }
+
+    fun readFromFile(fileName: String): InputStream {
+        val actualPath = File("/data/data/com.example.updatinglanguagedynamically/files", fileName)
+        val stream = FileInputStream(actualPath)
+        return try {
+            Log.d("moengage", "e.toString()")
+            stream
+        } catch (e: Exception) {
+            e.printStackTrace()
+            stream
+        }
+    }
 }
